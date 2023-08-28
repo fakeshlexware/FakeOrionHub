3015d9ac6d1cfaf952044ad50fc04ddf81480972
fakeshlex <140288115+fakeshlexware@users.noreply.github.com>
2023-08-29 00:01:12 +0200

    Create windowandtabs.lua

---
+++ windowandtabs.lua
@@ -0,0 +1,36 @@
+-- Making Window
+local Window = FakeOrionLib:MakeWindow({Name = "Fake Orion Hub!"})
+
+-- Making Tab
+local CreditsTab = Window:MakeTab({
+	Name = "Credits",
+	Icon = "rbxassetid://14605632862"
+})
+local LocalPlayerTab = Window:MakeTab({
+	Name = "LocalPlayer",
+	Icon = "rbxassetid://14604444609"
+})
+local MainTab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://14605350045"
+})
+local TrollingTab = Window:MakeTab({
+	Name = "Trolling",
+	Icon = "rbxassetid://14604663230"
+})
+local SettingsTab = Window:MakeTab({
+	Name = "Settings",
+	Icon = "rbxassetid://14604586243"
+})
+local LegendsOfSpeedTab = Window:MakeTab({
+	Name = "Legends Of Sp..",
+	Icon = "rbxassetid://14606890257"
+})
+local NaturalDisasterTab = Window:MakeTab({
+	Name = "Natural Disast..",
+	Icon = "rbxassetid://14606955137"
+})
+local MillionaireEmpireTab = Window:MakeTab({
+	Name = "Millionaire Em...",
+	Icon = "rbxassetid://14606988206"
+})
